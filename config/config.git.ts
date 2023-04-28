977f803f70941d8c67e11ffbef7cca8934732e36
cuijie <969423756@qq.com>
2023-04-28 09:58:09 +0800

    feat(workflow): config

---
+++ config/config.git.ts
@@ -0,0 +1,15 @@
+// https://umijs.org/config/
+import {defineConfig} from '@umijs/max';
+
+const { REACT_APP_ENV = 'git' } = process.env;
+export default defineConfig({
+  // base:'./',
+  // publicPath:'./',
+  headScripts: [
+    // 解决首次加载时白屏的问题
+    {
+      src: './scripts/loading.js',
+      async: true,
+    },
+  ],
+});
